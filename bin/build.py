f0b5b58942c2fda972be3320d01531116b1d18c3
Austin Goudge <austin@opensceneryx.com>
2006-11-21 16:04:17 +0000

    OpenSceneryX: Restructured build functions, added dimensions processing, added forest processing.

--- bin/build.py
+++ bin/build.py
@@ -1,6 +1,6 @@
 #!/usr/local/bin/python
 # Script to build a library release
-# Version: $Revision $
+# Version: $Revision$
 
 # Include common functions
 import functions
@@ -51,15 +51,18 @@ libraryFileHandle.write("LIBRARY\n")
 print "------------------------"
 print "Creating index.html"
 htmlIndexFileHandle = open("tags/" + version + "/index.html", "w")
-htmlIndexFileHandle.write("<html><head><title>OpenScenery Object Library for X-Plane</title>\n")
-htmlIndexFileHandle.write("<link rel='stylesheet' href='all.css' type='text/css'>\n")
+htmlIndexFileHandle.write("<html><head><title>OpenScenery Library for X-Plane</title>\n")
+htmlIndexFileHandle.write("<link rel='stylesheet' href='doc/all.css' type='text/css'>\n")
 htmlIndexFileHandle.write("<body>\n")
 htmlIndexFileHandle.write("<h1>OpenScenery Object Library for X-Plane</h1>\n")
 htmlIndexFileHandle.write("<div id='content'>\n")
+htmlIndexFileHandle.write("<p>The OpenScenery project is a collaborative effort by members of the <a href='http://www.x-plane.org'>X-Plane.org</a> community.  The aim is to provide a good range of scenery components as a library for use in scenery packages.</p>\n")
+htmlIndexFileHandle.write("<p>Thank you to everyone who has contributed so far!  If you would like to contribute some of your own objects to the library, visit <a href='http://svn.x-plugins.com/xplane/wiki/Scenery/Library'>the project pages</a> or <a href='http://forums.x-plane.org/index.php?showuser=2431'>contact me at the .org</a></p>\n")
 
 print "------------------------"
 print "Copying files"
 shutil.copyfile("trunk/support/all.css", "tags/" + version + "/doc/all.css")
+shutil.copyfile("trunk/support/cube.gif", "tags/" + version + "/doc/cube.gif")
 
 htmlIndexFileHandle.write("<h2>Objects</h2>\n")
 htmlIndexFileHandle.write("<ul>\n")
@@ -87,7 +90,12 @@ htmlIndexFileHandle.write("</ul>\n")
 
 print "------------------------"
 print "Finishing and closing files"
-htmlIndexFileHandle.write("</div></body></html>")
+htmlIndexFileHandle.write("</div>\n")
+file = open("trunk/support/_footer.html", "r")
+fileContents = file.read()
+file.close()
+htmlIndexFileHandle.write(fileContents)
+htmlIndexFileHandle.write("</body></html>")
 htmlIndexFileHandle.close()
 libraryFileHandle.close()
 
--- bin/functions.py
+++ bin/functions.py
@@ -2,44 +2,33 @@
 
 # functions.py
 # Common functions
-# Version: $Revision $
+# Version: $Revision$
 
 import os
 import shutil
 import re
 import urllib
 
+
 def handleObject(dirpath, filename, destinationRoot, libraryFileHandle, htmlIndexFileHandle):
   objectSourcePath = os.path.join(dirpath, filename)
-  print "handling object: " + objectSourcePath
   parts = dirpath.split("/", 2)
-  
-  if not os.path.isdir(os.path.join(destinationRoot, parts[2])):
-    os.makedirs(os.path.join(destinationRoot, parts[2]))
-    
-  shutil.copyfile(objectSourcePath, os.path.join(destinationRoot, parts[2], filename))
-  shutil.copyfile(os.path.join(dirpath, "info.txt"), os.path.join(destinationRoot, parts[2], "info.txt"))
-  shutil.copyfile(os.path.join(dirpath, "screenshot.jpg"), os.path.join(destinationRoot, parts[2], "screenshot.jpg"))
 
-  # open the object
+  print "Handling object: " + objectSourcePath
+  
+  # Set up paths and copy files
+  if not os.path.isdir(os.path.join(destinationRoot, parts[2])): os.makedirs(os.path.join(destinationRoot, parts[2]))
+  if not copySupportFiles(dirpath, destinationRoot, parts): return
+  shutil.copyfile(objectSourcePath, os.path.join(destinationRoot, parts[2], filename))
+
+  # Open the object
   file = open(objectSourcePath, "r")
   objectFileContents = file.readlines()
   file.close()
 
-  # open the info file
-  file = open(os.path.join(dirpath, "info.txt"))
-  infoFileContents = file.readlines()
-  file.close()
-  
-  # define the regex patterns:
+  # Define the regex patterns:
   texturePattern = re.compile("TEXTURE\s+(.*)")
   litTexturePattern = re.compile("TEXTURE_LIT\s+(.*)")
-  exportOverridePattern = re.compile("Export:\s+(.*)")
-  titlePattern = re.compile("Title:\s+(.*)")
-  authorPattern = re.compile("Author:\s+(.*)")
-  emailPattern = re.compile("Email:\s+(.*)")
-  dimensionsPattern = re.compile("Dimensions:\s+(.*)")
-  descriptionPattern = re.compile("Description:\s+(.*)")
 
   for line in objectFileContents:
     result = texturePattern.match(line)
@@ -54,61 +43,15 @@ def handleObject(dirpath, filename, destinationRoot, libraryFileHandle, htmlInde
       if os.path.isfile(textureFile):
         shutil.copyfile(textureFile, os.path.join(destinationRoot, parts[2], result.group(1)))
 
-  overrideVirtualPath = parts[2] + ".obj"
-  title = ""
-  author = ""
-  email = ""
-  dimensions = ""
-  description = ""
+  # Handle the info.txt file
+  virtualPaths = handleInfoFile(dirpath, destinationRoot, htmlIndexFileHandle, parts, ".obj")
   
-  for line in infoFileContents:
-    result = titlePattern.match(line)
-    if result:
-      title = result.group(1)
+  # Write to the library.txt file
+  for virtualPath in virtualPaths:
+    libraryFileHandle.write("EXPORT openscenery/" + virtualPath + " " + os.path.join(parts[2], filename) + "\n")
 
-    result = authorPattern.match(line)
-    if result:
-      author = result.group(1)
-      
-    result = emailPattern.match(line)
-    if result:
-      email = result.group(1)
 
-    result = dimensionsPattern.match(line)
-    if result:
-      dimensions = result.group(1)
 
-    result = descriptionPattern.match(line)
-    if result:
-      description = result.group(1)
-
-    result = exportOverridePattern.match(line)
-    if result:
-      overrideVirtualPath = os.path.join(dirpath, result.group(1)) + ".obj"
-      print "  Virtual path overridden: " + overrideVirtualPath
-      break
-
-  libraryFileHandle.write("EXPORT openscenery/" + overrideVirtualPath + "\n")
-  htmlIndexFileHandle.write("<li><a href='doc/" + urllib.pathname2url(title) + ".html" + "'>" + title + "</a></li>")
-  
-  htmlFileHandle = open(destinationRoot + "/doc/" + title + ".html", "w")
-  htmlFileHandle.write("<html><head><title>OpenScenery Object Library for X-Plane - " + title + "</title>\n")
-  htmlFileHandle.write("<link rel='stylesheet' href='../doc/all.css' type='text/css'>\n")
-  htmlFileHandle.write("<body>\n")
-  htmlFileHandle.write("<h1>OpenScenery Object Library for X-Plane</h1>\n")
-  htmlFileHandle.write("<div id='content'>\n")
-
-  htmlFileHandle.write("<h2>" + title + "</h2>\n")
-  htmlFileHandle.write("<img src='../" + os.path.join(parts[2], "screenshot.jpg") + "'>")
-  htmlFileHandle.write("<p><span class='fieldTitle'>Author:</span> <span class='fieldValue'>" + author + "</span></p>\n")
-  htmlFileHandle.write("<p><span class='fieldTitle'>Email:</span> <span class='fieldValue'>" + email + "</span></p>\n")
-  htmlFileHandle.write("<p><span class='fieldTitle'>Dimensions:</span> <span class='fieldValue'>" + dimensions + "</span></p>\n")
-  htmlFileHandle.write("<p><span class='fieldTitle'>Description:</span> <span class='fieldValue'>" + description + "</span></p>\n")
-  htmlFileHandle.write("<p><span class='fieldTitle'>File Path:</span> <span class='fieldValue'>" + objectSourcePath + "</span></p>\n")
-  htmlFileHandle.write("<p><span class='fieldTitle'>Virtual Path:</span> <span class='fieldValue'>" + overrideVirtualPath + "</span></p>\n")
-
-  htmlFileHandle.write("</div></body></html>")
-  htmlFileHandle.close()
 
 
 def handleFacade(dirpath, filename, destinationRoot, libraryFileHandle, htmlIndexFileHandle):
@@ -119,4 +62,179 @@ def handleFacade(dirpath, filename, destinationRoot, libraryFileHandle, htmlInde
 
 
 def handleForest(dirpath, filename, destinationRoot, libraryFileHandle, htmlIndexFileHandle):
-  print "forests not handled yet: " + os.path.join(dirpath, filename)
+  objectSourcePath = os.path.join(dirpath, filename)
+  parts = dirpath.split("/", 2)
+
+  print "Handling forest: " + objectSourcePath
+  
+  # Set up paths and copy files
+  if not os.path.isdir(os.path.join(destinationRoot, parts[2])): os.makedirs(os.path.join(destinationRoot, parts[2]))
+  if not copySupportFiles(dirpath, destinationRoot, parts): return
+  shutil.copyfile(objectSourcePath, os.path.join(destinationRoot, parts[2], filename))
+
+  # Open the object
+  file = open(objectSourcePath, "r")
+  objectFileContents = file.readlines()
+  file.close()
+
+  # Define the regex patterns:
+  texturePattern = re.compile("TEXTURE\s+(.*)")
+
+  for line in objectFileContents:
+    result = texturePattern.match(line)
+    if result:
+      textureFile = os.path.join(dirpath, result.group(1))
+      if os.path.isfile(textureFile):
+        shutil.copyfile(textureFile, os.path.join(destinationRoot, parts[2], result.group(1)))
+        break
+
+  # Handle the info.txt file
+  virtualPaths = handleInfoFile(dirpath, destinationRoot, htmlIndexFileHandle, parts, ".for")
+  
+  # Write to the library.txt file
+  for virtualPath in virtualPaths:
+    libraryFileHandle.write("EXPORT openscenery/" + virtualPath + " " + os.path.join(parts[2], filename) + "\n")
+
+
+
+
+
+def copySupportFiles(dirpath, destinationRoot, parts):
+  if not os.path.isfile(os.path.join(dirpath, "info.txt")):
+    print "  ERROR: No info.txt file found - object excluded"
+    return 0
+    
+  if not os.path.isfile(os.path.join(dirpath, "screenshot.jpg")):
+    print "  ERROR: No screenshot.jpg file found - object excluded"
+    return 0
+
+  shutil.copyfile(os.path.join(dirpath, "info.txt"), os.path.join(destinationRoot, parts[2], "info.txt"))
+  shutil.copyfile(os.path.join(dirpath, "screenshot.jpg"), os.path.join(destinationRoot, parts[2], "screenshot.jpg"))
+
+  return 1
+  
+  
+  
+  
+  
+def handleInfoFile(dirpath, destinationRoot, htmlIndexFileHandle, parts, suffix):
+   # open the info file
+  file = open(os.path.join(dirpath, "info.txt"))
+  infoFileContents = file.readlines()
+  file.close()
+  
+  # define the regex patterns:
+  texturePattern = re.compile("TEXTURE\s+(.*)")
+  litTexturePattern = re.compile("TEXTURE_LIT\s+(.*)")
+  exportPattern = re.compile("Export:\s+(.*)")
+  titlePattern = re.compile("Title:\s+(.*)")
+  authorPattern = re.compile("Author:\s+(.*)")
+  emailPattern = re.compile("Email:\s+(.*)")
+  urlPattern = re.compile("URL:\s+(.*)")
+  widthPattern = re.compile("Width:\s+(.*)")
+  heightPattern = re.compile("Height:\s+(.*)")
+  depthPattern = re.compile("Depth:\s+(.*)")
+  descriptionPattern = re.compile("Description:\s+(.*)")
+
+  # Define the variables to capture the data
+  virtualPaths = [parts[2] + suffix]
+  title = ""
+  author = ""
+  email = ""
+  url = ""
+  width = ""
+  height = ""
+  depth = ""
+  description = ""
+  
+  for line in infoFileContents:
+    result = titlePattern.match(line)
+    if result:
+      title = result.group(1)
+      continue
+
+    result = authorPattern.match(line)
+    if result:
+      author = result.group(1)
+      continue
+      
+    result = emailPattern.match(line)
+    if result:
+      email = result.group(1)
+      continue
+      
+    result = urlPattern.match(line)
+    if result:
+      url = result.group(1)
+      continue
+      
+    result = widthPattern.match(line)
+    if result:
+      width = result.group(1)
+      continue
+
+    result = heightPattern.match(line)
+    if result:
+      height = result.group(1)
+      continue
+
+    result = depthPattern.match(line)
+    if result:
+      depth = result.group(1)
+      continue
+
+    result = descriptionPattern.match(line)
+    if result:
+      description = result.group(1)
+      continue
+
+    result = exportPattern.match(line)
+    if result:
+      virtualPaths.append(result.group(1) + suffix)
+      print "  Additional virtual path added: " + result.group(1) + suffix
+      continue
+
+  htmlIndexFileHandle.write("<li><a href='doc/" + urllib.pathname2url(title) + ".html" + "'>" + title + "</a></li>")
+  
+  htmlFileHandle = open(destinationRoot + "/doc/" + title + ".html", "w")
+  htmlFileHandle.write("<html><head><title>OpenScenery Object Library for X-Plane - " + title + "</title>\n")
+  htmlFileHandle.write("<link rel='stylesheet' href='../doc/all.css' type='text/css'>\n")
+  htmlFileHandle.write("<body>\n")
+  htmlFileHandle.write("<h1>OpenScenery Object Library for X-Plane</h1>\n")
+  htmlFileHandle.write("<div id='content'>\n")
+
+  htmlFileHandle.write("<h2>" + title + "</h2>\n")
+
+  htmlFileHandle.write("<p class='virtualPath'>\n")
+  for virtualPath in virtualPaths:
+    htmlFileHandle.write(virtualPath + "<br />\n")
+  htmlFileHandle.write("</p>\n")
+  htmlFileHandle.write("<img class='screenshot' src='../" + os.path.join(parts[2], "screenshot.jpg") + "'>\n")
+  htmlFileHandle.write("<ul class='mainItemDetails'>\n")
+  if (not author == ""): htmlFileHandle.write("<li><span class='fieldTitle'>Author:</span> <span class='fieldValue'>" + author + "</span></li>\n")
+  if (not email == ""): htmlFileHandle.write("<li><span class='fieldTitle'>Email:</span> <span class='fieldValue'><a href='mailto:" + email + "'>" + email + "</a></span></li>\n")
+  if (not url == ""): htmlFileHandle.write("<li><span class='fieldTitle'>URL:</span> <span class='fieldValue'><a href='" + url + "'>" + url + "</a></span></li>\n")
+  if (not description == ""): htmlFileHandle.write("<li><span class='fieldTitle'>Description:</span> <span class='fieldValue'>" + description + "</span></li>\n")
+  
+  if (not width == "" and not height == "" and not depth == ""):
+    htmlFileHandle.write("<li><span class='fieldTitle'>Dimensions:</span>\n")
+    htmlFileHandle.write("<ul class='dimensions'>\n")
+    htmlFileHandle.write("<li id='width'><span class='fieldTitle'>w:</span> " + width + "</li>\n")
+    htmlFileHandle.write("<li id='height'><span class='fieldTitle'>h:</span> " + height + "</li>\n")
+    htmlFileHandle.write("<li id='depth'><span class='fieldTitle'>d:</span> " + depth + "</li>\n")
+    htmlFileHandle.write("</ul>\n")
+    htmlFileHandle.write("</li>\n")
+
+  htmlFileHandle.write("</ul>\n")
+
+  htmlFileHandle.write("</div>")
+
+  file = open("trunk/support/_footer.html", "r")
+  fileContents = file.read()
+  file.close()
+  htmlFileHandle.write(fileContents)
+
+  htmlFileHandle.write("</body></html>")
+  htmlFileHandle.close()
+  
+  return virtualPaths
