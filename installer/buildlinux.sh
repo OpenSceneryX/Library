01c1d16f93934e0bbae176ef4a8f4263a59f7419
Austin Goudge <austin@opensceneryx.com>
2008-05-09 16:58:15 +0000

    OpenSceneryX: Linux installer now packaged as a tarball instead of a zip

--- installer/buildlinux.sh
+++ installer/buildlinux.sh
@@ -3,4 +3,5 @@
 # Create a tarball of OpenSceneryX Installer but exclude resource forks
 # Mac OS 10.4 and earlier: export COPY_EXTENDED_ATTRIBUTES_DISABLE=true
 export COPYFILE_DISABLE=true
-zip -r OpenSceneryX\ Installer\ Linux.zip OpenSceneryX\ Installer
+tar -cf OpenSceneryX\ Installer\ Linux.tar OpenSceneryX\ Installer
+gzip OpenSceneryX\ Installer\ Linux.tar
