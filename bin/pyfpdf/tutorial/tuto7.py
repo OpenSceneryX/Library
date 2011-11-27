b910411638e6a263e26588c799b8ae8ed44986f4
Austin Goudge <austin@opensceneryx.com>
2011-11-27 19:31:01 +0000

    Added pyfpdf to build scripts

---
+++ bin/pyfpdf/tutorial/tuto7.py
@@ -0,0 +1,8 @@
+from fpdf import *
+
+pdf=FPDF()
+pdf.add_font('Calligrapher','','calligra.py')
+pdf.add_page()
+pdf.set_font('Calligrapher','',35)
+pdf.cell(0,10,'Enjoy new fonts with FPDF!')
+pdf.output('tuto7.pdf','F')
