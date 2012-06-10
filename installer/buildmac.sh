c61d7990c905296c1c897c6f2da864bab0c642fd
Austin Goudge <austin@opensceneryx.com>
2012-06-10 11:13:00 +0000

    Added code signing to Mac script.

--- installer/buildmac.sh
+++ installer/buildmac.sh
@@ -11,4 +11,14 @@ echo
 export COPYFILE_DISABLE=true
 
 cd Builds\ -\ Installer.rbvcp/Mac\ OS\ X\ \(Intel\)/
+
+# Code signing
+/usr/libexec/PlistBuddy -c "Delete :CFBundleIdentifier" OpenSceneryX\ Installer.app/Contents/Info.plist
+/usr/libexec/PlistBuddy -c "Delete :NSHumanReadableCopyright" OpenSceneryX\ Installer.app/Contents/Info.plist
+/usr/libexec/PlistBuddy -c "Delete :LSApplicationCategoryType" OpenSceneryX\ Installer.app/Contents/Info.plist
+/usr/libexec/PlistBuddy -c "Add :CFBundleIdentifier string 'com.AustinGoudge.OpenSceneryX'" OpenSceneryX\ Installer.app/Contents/Info.plist
+/usr/libexec/PlistBuddy -c "Add :NSHumanReadableCopyright string '© 2012, Austin Goudge'" OpenSceneryX\ Installer.app/Contents/Info.plist
+/usr/libexec/PlistBuddy -c "Add :LSApplicationCategoryType string 'public.app-category.simulation-games'" OpenSceneryX\ Installer.app/Contents/Info.plist
+codesign --signature-size 6400 -f -v -s 'Austin Goudge' OpenSceneryX\ Installer.app
+
 zip -r ../../OpenSceneryX-Installer-Mac.zip OpenSceneryX\ Installer.app
