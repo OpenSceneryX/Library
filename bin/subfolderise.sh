e57b06e8455af385c7f870dcb53b8db04a155df9
aussig <austin@opensceneryx.com>
2018-09-02 14:48:03 +0100

    subfolderise shell script now replaces the TEXTURE definition with a specified one and reports all unique textures used.

--- bin/subfolderise.sh
+++ bin/subfolderise.sh
@@ -1,28 +1,36 @@
 #!/bin/bash
 # This script walks through every x-plane file in a given folder, creates a subfolder, incrementally numbered
-# and moves the file into the folder.  In addition, it copies a template info.txt file into each
-# subfolder, and it expects this template info.txt file to be in the same folder.  Finally, it generates a
-# screenshot if it's a .obj
+# and moves the x-plane file into the folder.  It re-writes the TEXTURE definition to replace the texture path
+# with one specified as an argument. In addition, it copies a template info.txt file into each
+# subfolder, and it expects this template info.txt file to be in the same folder. It generates a
+# screenshot if it's a .obj and finally it reports all unique textures used.
 
 SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
-MYDIR=$1
+TEXTUREPATH=$1
+MYDIR=$2
 
-echo "Script dir: " $SCRIPTDIR
-echo "Working dir: " $MYDIR
+if [ -z TEXTUREPATH ]
+then
+    echo "Usage: subfolderise relative-texture-path path"
+    exit
+fi
 
-echo "========================"
 if [ -z $MYDIR ] || [[ ! -d $MYDIR ]]
 then
-    echo "Passed argument is not a directory, cannot continue"
+    echo "Usage: subfolderise relative-texture-path path"
     exit
 elif [[ ! -f $MYDIR/info.txt ]]
 then
-    echo "No template info.txt file found, cannot continue"
+    echo "No template info.txt file found at path, cannot continue"
     exit
 else
     echo "Processing $MYDIR"
     cd $MYDIR
 fi
+
+echo "========================"
+echo "Script dir: " $SCRIPTDIR
+echo "Working dir: " $MYDIR
 echo "========================"
 
 # Find the highest numbered subfolder that already exists
@@ -32,6 +40,13 @@ while [[ -d "$MYDIR/$N" ]] ; do
 done
 
 # N is now the next incremental folder number
+
+TEXTUREPATHS=()
+
+# Store the input file separator and set it to newline.  Need to do this so we can store items containing
+# spaces in our array
+OLDIFS="$IFS"
+IFS=$'\n'
 
 for F in $(find . -type f -maxdepth 1 -name '*.fac' -o -name '*.for' -o -name '*.lin' -o -name '*.obj' -o -name '*.pol')
 do
@@ -44,24 +59,42 @@ do
 
     case $F in
         *fac)
-            mv $F $N/facade.fac
+            NEWFILE=$N/facade.fac
+            cp $F $NEWFILE
             ;;
         *for)
-            mv $F $N/forest.for
+            NEWFILE=$N/forest.for
+            cp $F $NEWFILE
             ;;
         *lin)
-            mv $F $N/line.lin
+            NEWFILE=$N/line.lin
+            cp $F $NEWFILE
             ;;
         *obj)
-            mv $F $N/object.obj
+            NEWFILE=$N/object.obj
+            cp $F $NEWFILE
             # It's a .obj file, generate screenshot
-            $SCRIPTDIR/generate_screenshots.sh $MYDIR/$N/
+            #$SCRIPTDIR/generate_screenshots.sh $MYDIR/$N/
             ;;
         *pol)
-            mv $F $N/polygon.pol
+            NEWFILE=$N/polygon.pol
+            cp $F $NEWFILE
             ;;
         *)
     esac
 
+    # Replace the texture reference
+    sed -i '' -E -e "s|TEXTURE[[:space:]]+.*\/([^/]+\.[A-Za-z]{3})|TEXTURE $TEXTUREPATH/\1|" $NEWFILE
+    TEXTUREPATHS+=($(grep -E "TEXTURE[[:space:]]+.*\/([^/]+\.[A-Za-z]{3})" $NEWFILE))
+
     N=$(($N+1))
 done
+
+# Restore the old input file separator
+IFS="$OLDIFS"
+
+echo "========================"
+echo "Unique textures in this set of files:"
+eval TEXTUREPATHS=($(printf "%q\n" "${TEXTUREPATHS[@]}" | sort -u))
+printf "%s\n" "${TEXTUREPATHS[@]}"
+echo "========================"
