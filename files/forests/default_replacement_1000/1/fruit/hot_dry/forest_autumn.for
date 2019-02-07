ec795919752c3966b276521128ac12bc4b67f085
aussig <austin@opensceneryx.com>
2019-02-07 19:02:36 +0000

    Add seasonal variants for all FlyAgi's tree forests

---
+++ files/forests/default_replacement_1000/1/fruit/hot_dry/forest_autumn.for
@@ -0,0 +1,23 @@
+A
+800
+FOREST
+
+TEXTURE ../../../../../shared_textures/flyagi/autumn/Forests_01.png
+
+LOD 15000.000000
+
+SCALE_X 4096
+SCALE_Y 4096
+
+SPACING 7.00 7.00
+RANDOM 5.00 5.00
+
+#	low-left	tex size	center	percent	--height--
+# tree	s	t	w	y	offset	occur	min	max	quads	type	name
+#------------------------------------------------------------------------------------------
+TREE	1024	0		1024	1024	477	30	4	10	2	0	Fruit
+TREE	0		1024	1024	1024	478	20	6	12	2	0	Tree5
+TREE	2048	1024	1024	1024	512	20	6	12	2	0	Tree7
+TREE 	2048	392		512		307		256	30	2	6	2	0	Shrub6
+
+SKIP_SURFACE water
---
+++ files/forests/default_replacement_1000/1/wetland_shrub/very_cold_dry/forest_autumn.for
@@ -0,0 +1,31 @@
+A
+800
+FOREST
+
+TEXTURE ../../../../../shared_textures/flyagi/autumn/Forests_01.png
+
+LOD 15000.000000
+
+SCALE_X 4096
+SCALE_Y 4096
+
+SPACING 15.00 15.00
+RANDOM 7.00 7.00
+
+#	low-left	tex size	center	percent	--height--
+# tree	s	t	w	y	offset	occur	min	max	quads	type	name
+#------------------------------------------------------------------------------------------
+TREE	0		2048	1024	1024	480	7	4	10	2	0	Tree1
+TREE	1024	2048	1024	1024	512	6	4	10	2	0	Tree2
+TREE	0		3072	1024	1024	535	6	4	10	2	0	Tree3
+TREE	1024	3072	1024	1024	512	6	4	10	2	0	Tree4
+TREE	2048	2048	1024	1024	508	7	4	10	2	0	Coni1
+TREE	3072	2048	1024	1024	512	6	4	10	2	0	Coni2
+TREE	2048	3072	1024	1024	556	6	4	10	2	0	Coni3
+TREE	3072	3072	1024	1024	492	6	4	10	2	0	Coni4
+TREE 	0		0		512		512		288	13	2	6	2	0	Shrub1
+TREE 	512		0		512		512		311	13	2	6	2	0	Shrub2
+TREE 	0		512		512		512		256	12	2	6	2	0	Shrub3
+TREE 	512		512		512		512		256	12	2	6	2	0	Shrub4
+
+SKIP_SURFACE water
