929fe9db1983e3abda42da4ebf015a4f57dde3bf
aussig <austin@opensceneryx.com>
2019-05-01 18:48:49 +0100

    Replaced deep winter texture and created new terramaxx deep winter season using the old bluer texture.

---
+++ files/forests/default_replacement_1000/1/broadleaf_dense/hot_dry/forest_winter_terramaxx_deep_snow.for
@@ -0,0 +1,24 @@
+A
+800
+FOREST
+
+TEXTURE ../../../../../shared_textures/flyagi/winter_terramaxx_deep_snow/Forests_01.png
+NO_SHADOW
+
+LOD 15000.000000
+
+SCALE_X 4096
+SCALE_Y 4096
+
+SPACING 17.00 17.00
+RANDOM 6.00 6.00
+
+#	low-left	tex size	center	percent	--height--
+# tree	s	t	w	y	offset	occur	min	max	quads	type	name
+#------------------------------------------------------------------------------------------
+TREE 	2048	0		512		386		256	25	2	8	2	0	Shrub5
+TREE 	2048	392		512		307		256	25	2	8	2	0	Shrub6
+TREE 	2048	705		512		307		256	25	2	8	2	0	Shrub7
+TREE 	2560	0		512		492		256	25	2	8	2	0	Shrub8
+
+SKIP_SURFACE water
