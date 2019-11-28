b01eda30ddbdc7c67105dae30fbf95ba0883df1f
aussig <austin@opensceneryx.com>
2019-11-28 14:28:54 +0000

    Add new hedge, with variants, from FlyAgi Vegetation update.  #65.

---
+++ files/forests/plants/laurus/2/0.9-1.0m/forest_winter_terramaxx_deep_snow.for
@@ -0,0 +1,27 @@
+A
+800
+FOREST
+
+TEXTURE ../../../../../shared_textures/flyagi/winter_terramaxx_deep_snow/Hedge_01.png
+NO_SHADOW
+
+LOD 1000
+
+SCALE_X 4096
+SCALE_Y 4096
+
+SPACING 0.3 0.3
+RANDOM 0.2 0.2
+
+#	low-left	tex size	center	percent	--height--
+# tree	s	t	w	y	offset	occur	min	max	quads	type	name
+#------------------------------------------------------------------------------------------
+TREE	0	3072	512	1024	256	50	0.9	1.0	2	0	hedge
+TREE	512	3072	512	1024	256	50	0.9	1.0	2	0	hedge
+
+
+
+
+
+
+
--- files/forests/plants/laurus/2/1.8-2.0m/forest_winter_snow.for
+++ files/forests/plants/laurus/2/1.8-2.0m/forest_winter_snow.for
@@ -2,23 +2,26 @@ A
 800
 FOREST
 
-TEXTURE Vine.png
+TEXTURE ../../../../../shared_textures/flyagi/winter_snow/Hedge_01.png
 NO_SHADOW
 
 LOD 1000
 
-SCALE_X 1024				
-SCALE_Y 1024
+SCALE_X 4096
+SCALE_Y 4096
 
-SPACING 3.0 1.5
-RANDOM 0.0 0.0
+SPACING 0.7 0.7
+RANDOM 0.2 0.2
 
 #	low-left	tex size	center	percent	--height--
 # tree	s	t	w	y	offset	occur	min	max	quads	type	name
 #------------------------------------------------------------------------------------------
-TREE	0	0	1024	1024	512	100	2.5	2.5	1	0	fruit
+TREE	0	3072	512	1024	256	50	1.8	2.0	2	0	hedge
+TREE	512	3072	512	1024	256	50	1.8	2.0	2	0	hedge
 
-SKIP_SURFACE water
+
+
+
 
 
 
