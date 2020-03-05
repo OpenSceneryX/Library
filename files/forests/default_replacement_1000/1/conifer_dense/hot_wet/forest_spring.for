108bf5b01dec7d9664c2a639e8d7e4f114a75b36
aussig <austin@opensceneryx.com>
2020-03-05 16:53:29 +0000

    Add spring variants for all SAM SDK vegetation.

---
+++ files/forests/default_replacement_1000/1/conifer_dense/hot_wet/forest_spring.for
@@ -0,0 +1,25 @@
+A
+800
+FOREST
+
+TEXTURE Forests_01.png
+NO_SHADOW
+
+LOD 15000.000000
+
+SCALE_X 4096
+SCALE_Y 4096
+
+SPACING 9.00 9.00
+RANDOM 5.00 5.00
+
+#	low-left	tex size	center	percent	--height--
+# tree	s	t	w	y	offset	occur	min	max	quads	type	name
+#------------------------------------------------------------------------------------------
+TREE	2048	2048	1024	1024	508	20	18	30	2	0	Coni1
+TREE	3072	2048	1024	1024	512	20	18	30	2	0	Coni2
+TREE	2048	3072	1024	1024	556	20	18	30	2	0	Coni3
+TREE	3072	3072	1024	1024	492	20	18	30	2	0	Coni4
+TREE	1536	1024	512		1024	256	20	18	30	2	0	Juniper2
+
+SKIP_SURFACE water
